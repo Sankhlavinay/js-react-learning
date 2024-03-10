f699873acc33eb4735577f5cc4bfb4f63fd72814
Sankhlavinay <109958392+Sankhlavinay@users.noreply.github.com>
2024-03-10 10:59:43 +0000

    leaen strings

---
+++ comparison/01.js
@@ -0,0 +1,8 @@
+const arr = [1,2,3,4]
+const myObj = {
+    name:"vinay",
+    age: 21
+}
+
+console.log(typeof myObj);
+console.log(myObj);
---
+++ string/01.js
@@ -0,0 +1,16 @@
+const name = "vinay";
+const lastName = new String("sankhla")
+const broName = "    raaj   vinay"
+
+// console.log(lastName[1]);
+// console.log(lastName.length);
+// console.log(lastName.toUpperCase());
+// console.log(lastName.charAt(3));
+// console.log(lastName.indexOf("a"));
+// console.log(lastName.substring(0,4));
+// console.log(lastName.slice(-5, 4));
+// console.log(broName.trim());    //remove white spaces in string
+// console.log(broName.trimStart());
+// console.log(broName.trimEnd());    
+
+// console.table([name, lastName]);
