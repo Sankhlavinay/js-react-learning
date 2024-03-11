82dc011b5d8beb08aeef289c59164808e0f6760b
Sankhlavinay <109958392+Sankhlavinay@users.noreply.github.com>
2024-03-11 06:34:24 +0000

    Num and Math

---
+++ basics/num_and_maths.js
@@ -0,0 +1,30 @@
+const balance = 600;
+// console.log(balance.toString())
+// console.log(typeof balance.toString())   Convert in string
+// console.log(balance.toFixed(2))  Fixed after decimal
+
+const otherNum = 123.95656
+// console.log(balance.toPrecision(5)) Total value from starting
+
+const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'))
+
+// There many more methods are here like MAX_VALUE and MIN_VALUE
+
+
+// +++++++++++Maths+++++++++++++++
+// console.log(Math.abs(-40))  it change -ve value into +ve value
+// console.log(Math.round(4.4))  It gives round of value
+// console.log(Math.ceil(4.2))  It gives bigger value in round off 
+// console.log(Math.floor(4.9))   It gives smaller value in round off
+// console.log(Math.random())  it gives value btw 0 to 1
+// console.log((Math.random()*10) + 1)  shift value to left (*10), for reduncy 0 add (+1)
+// console.log(Math.floor(Math.random()*10) + 1)
+
+
+//Dice game
+
+const min = 1
+const max = 6
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)  Formula 
